d5bd278cce7701423a22c6870033c45d9dd9b0b0
minhhang411 <maominhhang114@gmail.com>
2025-10-24 23:06:58 +0700

    (#46) add missing supplier contract files

---
+++ backend/src/Services/Interfaces/ISupplierService.cs
@@ -0,0 +1,8 @@
+public interface ISupplierService
+{
+    Task<IEnumerable<SupplierResponse>> GetSuppliersAsync();
+    Task<SupplierResponse?> GetSupplierByIdAsync(int id);
+    Task<SupplierResponse> AddSupplierAsync(CreateSupplierRequest createSupplierRequest);
+    Task<SupplierResponse?> UpdateSupplierAsync(int id, UpdateSupplierRequest updateSupplierRequest);
+    Task<bool> DeleteSupplierAsync(int id);
+}
