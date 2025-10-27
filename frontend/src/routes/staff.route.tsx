5aded0d93e39d956e04fa8c32ad52c65703b6518
buitrongtan91 <buitrongtan.work@gmail.com>
2025-10-27 19:19:58 +0700

    #47 feat(staff): complete design ui make order in staff

--- frontend/src/routes/staff.route.tsx
+++ frontend/src/routes/staff.route.tsx
@@ -1,10 +1,14 @@
 import type { RouteObject } from "react-router-dom";
 import ManageCustomer from "../pages/admin/manage-customer";
+import Sale from "../pages/staff/sale";
 
 export const staffRoutes: RouteObject[] = [
   {
     path: "customers",
     element: <ManageCustomer />,
   },
-  
+  {
+    path: "sale",
+    element: <Sale />,
+  },
 ];
