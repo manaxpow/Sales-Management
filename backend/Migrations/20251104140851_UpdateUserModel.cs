4f4a0efc25a08cc2bd4979ec7db24eff018a7fb9
QuangThangt <149249812+QuangThangt@users.noreply.github.com>
2025-11-08 19:37:12 +0700

    #65 Resolve conflicts

---
+++ backend/Migrations/20251104140851_UpdateUserModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "ENUM('admin','staff')",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Role",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "ENUM('admin','staff')")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
