548ff8bccde9a0ac4596a1491b267d6b8618e919
Trần Nam <namtran96hth@gmail.com>
2025-11-28 01:26:17 +0700

    #89 - design home page

---
+++ blazor_web/Models/Product.cs
@@ -0,0 +1,13 @@
+
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public int SupplierId { get; set; }
+        public string ProductName { get; set; } = "";
+        public string Barcode { get; set; } = "";
+        public decimal Price { get; set; }
+        public string Unit { get; set; } = "pcs";
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+
