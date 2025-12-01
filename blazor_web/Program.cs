3d1855cc625b3b7f8ab5cb8d1474441fda3c9990
manaxpow <157128585+manaxpow@users.noreply.github.com>
2025-12-01 22:56:35 +0700

    #93 design mamange customer with blazor (#94)

--- blazor_web/Program.cs
+++ blazor_web/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddScoped<CustomerService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
